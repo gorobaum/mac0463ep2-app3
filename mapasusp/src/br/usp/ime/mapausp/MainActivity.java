3591e0b901d7ae38d4e799cdc4ee1b2a2aa86268
Thiago Nunes <gorobaum@gmail.com>
2013-05-25 16:37:39 -0300

    PAGODÃO NA LAJE

--- mapasusp/src/br/usp/ime/mapausp/MainActivity.java
+++ mapasusp/src/br/usp/ime/mapausp/MainActivity.java
@@ -2,7 +2,6 @@ package br.usp.ime.mapausp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 
 public class MainActivity extends Activity {
 	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -11,13 +10,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		new PagodeMAster().execute("http://uspservices.deusanyjunior.dj/categoriaslocal.json");
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
 }
